cc0f3b4515205579735d4d3126ce93a3728f2cad
Luke Mcildoon <luke@twofiftyfive.net>
2011-09-13 21:36:23 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daikanbana::Application.config.secret_token = '42ce1818b9d34308a8aed6a71d855c0c31c483267e644fa8034ff343bb93633cce1d7f3d4cedaf32ed7821ee72c82cb20b88656a01830cc0cd2c7ccfe0257a8e'
